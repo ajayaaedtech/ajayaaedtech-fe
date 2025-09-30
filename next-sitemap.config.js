ae43f0f8f2bbdc7e19046257991ba7b8ba1fa6af
Mohan Singh Mahecha <mohansinghmahecha2000@gmail.com>
2025-09-30 21:43:00 +0530

    its live

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ajayaaedtech.com', // your domain
+  generateRobotsTxt: true, // (optional) will generate robots.txt too
+  // ...other options
+}
