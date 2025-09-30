79cbf44a1495e3307a5a06585d1cfd74e9af65bd
Mohan Singh Mahecha <mohansinghmahecha2000@gmail.com>
2025-09-30 22:14:45 +0530

    try

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://ajayaaedtech.com', // your domain
-  generateRobotsTxt: true, // (optional) will generate robots.txt too
-  // ...other options
-}
+  siteUrl: 'https://ajayaaedtech.com',
+  generateRobotsTxt: false,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    // **disable the Host line**
+    additionalSitemaps: [], // leave empty
+    // This tells next-sitemap to NOT include Host
+    host: undefined,
+  },
+};
